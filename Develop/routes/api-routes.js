0dd049c175c697a3504ccb091354d8f349e61c03
PROX-E <lichlord18@gmail.com>
2020-03-13 21:01:02 -0500

    create note post function

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -7,4 +7,14 @@ module.exports = function(app) {
       res.send(db);
     });
 
+    app.post("/api/notes", function(req, res) {
+        let noteId = uuid();
+        
+        let newNote = {
+        id: noteId,
+        title: req.body.title,
+        text: req.body.text
+        };
+    })
+
 };
