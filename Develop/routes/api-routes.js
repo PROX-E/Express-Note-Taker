4deca7096d4f0549cabbd034e28e4450a78197d7
PROX-E <lichlord18@gmail.com>
2020-03-13 20:55:35 -0500

    get api function

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,3 +1,8 @@
 const db = require("../db/db.json");
 const fs = require("fs");
 const uuid = require("uuid/v4");
+
+module.exports = function(app) {
+    app.get("/api/notes", function(req, res) {
+      res.send(db);
+    });
