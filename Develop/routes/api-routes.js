9b8ee8bb36e21e366b286f5987f2110d539b06f5
PROX-E <lichlord18@gmail.com>
2020-03-13 21:14:14 -0500

    create note function, Delete note function

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -15,6 +15,36 @@ module.exports = function(app) {
         title: req.body.title,
         text: req.body.text
         };
-    })
+    
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+          const allNotes = JSON.parse(data);
+      
+      allNotes.push(newNote);
+      
+       fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), err => {
+          if (err) throw err;
+                res.send(db);
+          console.log("Note created!")   
+         });
+      });
+    });
 
+    app.delete("/api/notes/:id", (req, res) => {
+
+        let noteId = req.params.id;
+    
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+          if (err) throw err;
+    
+          const allNotes = JSON.parse(data);
+          const newAllNotes = allNotes.filter(note => note.id != noteId);
+    
+          fs.writeFile("./db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
+            if (err) throw err;
+            res.send(db);
+            console.log("Note deleted!")
+          });
+        });
+      });
 };
